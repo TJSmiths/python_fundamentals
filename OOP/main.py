b760bb65eb51614eec500257421cb933e65468fa
Thomas Smith <tom@tjsmiths.co.uk>
2024-12-09 11:12:43 +0000

    added main.py

---
+++ OOP/main.py
@@ -0,0 +1,6 @@
+from person import Person
+
+liam = Person("Jeff", 30, "Tall")
+tom = Person("Tom", 29, "Medium at best")
+
+print(f"My name is {tom.name}, I am {tom.age} years old and my height is {tom.height}")
