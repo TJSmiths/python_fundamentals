c27bb725223fb4a8d30974d17539aebcb6800702
Thomas Smith <tom@tjsmiths.co.uk>
2024-12-09 11:38:06 +0000

    added heroclass and superheroes

---
+++ OOP/heroclass.py
@@ -0,0 +1,6 @@
+class Superhero():
+    def __init__(self, name, identity, power, arch_enemy):
+        self.name = name
+        self.identity = identity
+        self.power = power
+        self.arch_enemy = arch_enemy
